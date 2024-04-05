cc8d51a447d38a825e058552e30afa59653465c3
kingranzz <156513688+kingranzz@users.noreply.github.com>
2024-04-06 01:12:42 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.ytname = "YT: @ranzstartboyy" //ur yt chanel name
+global.socialm = "GitHub: ranzganteng" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = 'RanZbot WhatsApp' //ur bot name
+global.ownernumber = '6282393734303' //ur owner number
+global.ownername = '🦄드림 가이 Ranz Tamfan😎' //ur owner name
+global.websitex = "https://xnxx.com/tobrutt"
+global.wagc = "https://whatsapp.com/channel/"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "Ranz Bot Inc."
+global.botscript = 'https://wa.me/6282393734303' //script link
+global.packname = "ranz suka tobrutt😎"
+global.author = "🦄\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.creator = "6282393734303@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["6282393734303"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -40,7 +40,7 @@ global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
+global.anticall = true //bot blocks user when called
 global.autoswview = false //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
